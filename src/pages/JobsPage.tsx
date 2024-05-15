a8c70cc427f275e4ee6f402c91bda8fbd8310faa
Imre <imre.geczy@yahoo.co.uk>
2024-05-15 19:50:10 +0100

    main reformat and made tidy

--- src/pages/JobsPage.tsx
+++ src/pages/JobsPage.tsx
@@ -1,11 +1,12 @@
-import { JobList } from "../components/JobList"
+import React from 'react';
+import { JobList } from '../components/JobList';
 
 const JobsPage = () => {
-  return (
-      <section className="bg-blue-50 px-4 py-6">
-          <JobList />
-    </section>
-  )
-}
+    return (
+        <section className="bg-blue-50 px-4 py-6">
+            <JobList />
+        </section>
+    );
+};
 
-export default JobsPage
+export default JobsPage;
