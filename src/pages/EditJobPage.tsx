947b4a8c6b43f23d77c330920e268b9784ea3bc4
Imre <imre.geczy@yahoo.co.uk>
2024-05-16 00:30:48 +0100

    feature/5 remove unused react lib

--- src/pages/EditJobPage.tsx
+++ src/pages/EditJobPage.tsx
@@ -9,7 +9,7 @@ const EditJobPage = ({ updateJobSubmit }) => {
         <section className='bg-indigo-50'>
             <div className='container m-auto max-w-2xl py-24'>
                 <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-                    <DetailsForm jobSubmit={updateJobSubmit} job={job} serviceTitle='Update Job' id={id} />
+                    <DetailsForm jobSubmit={updateJobSubmit} serviceTitle='Update Job' job={job} id={id} />
                 </div>
             </div>
         </section>
